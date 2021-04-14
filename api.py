58efc1390e4474c39f8681e19d5e1014c0ad0323
daikichidaze <dkch.yoshioka.t@gmail.com>
2021-04-14 14:37:52 +0700

    first minimal API

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask, app
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
